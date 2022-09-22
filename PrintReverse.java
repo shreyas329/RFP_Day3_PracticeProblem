59e70f1e06ae1a654305fa1f8e6b3cd853e89af1
Shreyas Sharma P <57483308+shreyas329@users.noreply.github.com>
2022-09-22 22:02:38 +0530

    Printing the elements of array in reverse order

---
+++ PrintReverse.java
@@ -0,0 +1,20 @@
+package day3;
+
+public class PrintReverse {
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		System.out.println("Array");
+		for(int i=0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]+", "); 
+		}
+		System.out.println("\nRverse of the array:");
+		for(int i=arr.length-1; i>=0; --i)
+		{
+			System.out.print(arr[i]+", "); 
+		}
+
+	}
+
+}
