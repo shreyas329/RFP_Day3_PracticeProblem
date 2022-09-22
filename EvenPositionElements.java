6d047e27e9b2f8a0e90368682340dcfd5cd7062c
Shreyas Sharma P <57483308+shreyas329@users.noreply.github.com>
2022-09-22 21:55:43 +0530

    Program to print the element in even position.

---
+++ EvenPositionElements.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class EvenPositionElements {
+
+	public static void main(String[] args) {
+		int[] arr = {15, 12, 5, 6, 18, 65, 35, 67};
+		for(int i=0; i<arr.length; i++)
+		{
+			if(i%2 == 0)
+			{
+				System.out.print(arr[i]+" ,");
+			}
+		}
+		
+	}
+
+}
